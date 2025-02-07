023f8edd5ea8594ea7fa6897370f8d9a0b702fb3
zhuff99 <zhuff99@gmail.com>
2025-02-06 17:20:45 -0800

    adding initial files

---
+++ git_commands.sh
@@ -0,0 +1,8 @@
+git init
+git add .
+git commit -m "adding initial files"
+git branch -M main
+git remote add origin https://github.com/zhuff99/LinkedListCPP
+git push --set-upstream origin main
+echo "All done! Press ENTER to close."
+read
