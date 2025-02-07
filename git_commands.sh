c22c6105708501721ac6c135f5401136630f9ab1
zhuff99 <zhuff99@gmail.com>
2025-02-06 17:22:17 -0800

    adding initial files

--- git_commands.sh
+++ git_commands.sh
@@ -1,7 +1,7 @@
 git init
 git add .
 git commit -m "adding initial files"
-git remote add origin https://github.com/zhuff99/LinkedListCPP
+git remote add origin https://github.com/zhuff99/VectorSorting
 git push --set-upstream origin main
 echo "All done! Press ENTER to close."
 read
