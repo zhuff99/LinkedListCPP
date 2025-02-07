82d44287c1398fc85cfba34bf1dfa748db809d1c
zhuff99 <zhuff99@gmail.com>
2025-02-06 17:23:59 -0800

    adding initial files

--- git_commands.sh
+++ git_commands.sh
@@ -2,6 +2,7 @@ git init
 git add .
 git commit -m "adding initial files"
 git remote add origin https://github.com/zhuff99/VectorSorting
+git pull
 git push --set-upstream origin main
 echo "All done! Press ENTER to close."
 read
